6fa54ee1bfc6baae4194128d85f4999561bae487
Bruno <157668367+Bruno2657@users.noreply.github.com>
2024-02-18 04:26:11 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,29 @@
-const botao = document.querySelector(".btn-plataforma");
-const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
-
-botao.addEventListener("click", () => {
-    elementoPlataformas.classList.toggle("ativo");
+const button = document.querySelector('.epidemic-z');
+const content = document.getElementById('epidemic-z-info');
+button.addEventListener('click', function(ativo) {
+    if (content.style.display === 'none') {
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const botoes = document.querySelectorAll('.gl');
-    const modal = document.getElementById('modal');
-    const imagemAmpliada = document.getElementById('imagem-ampliada');
-    const fecharModal = document.querySelector('.fechar');
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona o botão
+    const buttonRedeDragon = document.querySelector('.RedeDragon');
+    // Seleciona o conteúdo a ser mostrado/ocultado
+    const contentRedeDragon = document.getElementById('RedeDragon-info');
 
-    botoes.forEach(function (botao) {
-        botao.addEventListener('click', function () {
-            modal.style.display = 'block';
-            const imagemSrc = botao.querySelector('img').src;
-            imagemAmpliada.src = imagemSrc;
-        });
-    });
-
-    fecharModal.addEventListener('click', function () {
-        modal.style.display = 'none';
-    });
-
-    window.addEventListener('click', function (event) {
-        if (event.target === modal) {
-            modal.style.display = 'none';
+    // Adiciona um listener para o evento de clique no botão
+    buttonRedeDragon.addEventListener('click', function() {
+        // Verifica se o conteúdo está visível
+        if (contentRedeDragon.style.display === 'none' || contentRedeDragon.style.display === '') {
+            // Se estiver oculto, torna-o visível
+            contentRedeDragon.style.display = 'block';
+        } else {
+            // Se estiver visível, torna-o oculto
+            contentRedeDragon.style.display = 'none';
         }
     });
 });
+
